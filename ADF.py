3b22c027f2657277ad3931f319681de70b98103b
Jakariya404 <111103375+Jakariya404@users.noreply.github.com>
2022-08-15 11:55:09 +0600

    Create ADF.py

---
+++ ADF.py
@@ -0,0 +1,62 @@
+
+def logo():
+ print("""%s
+  _____      _____      ______ 
+ (_____)    (_____)    (______)
+(_)___(_)   (_)  (_)   (_)__   
+(_______)   (_)  (_)   (____)  
+(_)   (_)   (_)__(_)   (_)     
+(_)   (_)   (_____)    (_)     
+"""%(O))
+
+def bsn_menu():
+    os.system('clear')
+def hasil(OK,cp):
+    if len(OK) != 0 or len(cp) != 0:
+        print('\n----------------------------------------------')
+        print(' Your Process Complete...')
+        print('----------------------------------------------')
+        print(' [%s+%s] \033[1;97mTOTAL OK : %s --- \033[1;97mAdf-ok.txt'%(O,O,str(len(ok))))
+        print(' [%s+%s] \033[1;97mTOTAL CP : %s --- \033[1;97mAdf-cp.txt'%(O,O,str(len(cp))))
+        print('----------------------------------------------')
+        input(f"\n\033[1;97m Press Enter To Go Back ")
+        bsn_menu()
+
+def bsn_menu():
+    os.system('clear')
+    logo()
+    ipm = requests.get(url_ip).json() 
+    IP = ipm["origin"]
+    print(" [*]. CREATED BY |:-( A D F )");time.sleep (0.03)
+    print(" [*]. Tysm To My All Frendz. (Waqar BILAL)");time.sleep (0.03)
+    print(" [*] -----------------------------------------------------------------------------------");time.sleep (0.03)
+    print(" [*]. VERSION  1.0                                    ");time.sleep (0.03)   
+    print(" [*]. WP             +923491383368       ");time.sleep (0.03)
+    print(" [*]. This Tool was made in Pakistan                 ");time.sleep (0.03)
+    print(" [*] -----------------------------------------------------------------------------------");time.sleep (0.03)
+    print(".[*] ---------------------------------------------");time.sleep(0.03)
+    print(" [*] A N O N Y M O U S  D A R K F A T E  ( A D F )    ");time.sleep (0.03)
+    print(".[*] ---------------------------------------------");time.sleep(0.03)
+    print(".[*] This is a Premium Tool Bro                                 ");time.sleep (0.03)
+    print(".[*] You neeed to buy this tool for 1 month membership                       ");time.sleep (0.03) 
+    print(" [*] ---------------------------------------------");time.sleep(0.03)
+    print(" [*] IP ADDRESS        [%s]\n"%(IP));time.sleep(0.01)
+    print("   \033[1;97m              Menu")
+    print("-----------------=\<------------------")
+    print(" [1] File Cloning")
+    print(" [2] Follow Owner")
+    print(" [0] Exit")
+    print("")
+    pepek = input(' Select : ')
+    if pepek in['1','01']:
+        __bsn__().bilo(id)
+            
+
+class __bsn__:
+
+    def __init__(self):
+        self.id = []
+
+    def bilo(self,id):
+        os.syste
+      
